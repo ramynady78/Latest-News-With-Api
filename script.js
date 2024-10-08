6bee0847b61e44b334c442cd04787b40a8fa3f59
Ramy Nady <andmohamedramy@gmail.com>
2024-10-08 01:15:06 -0700

    eddit js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     })
     function fetchNews(){
-        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}&pageSize=${pageSize}`
+        const url = `http://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}&pageSize=${pageSize}`
         fetch(url)
         .then(respone => respone.json())
         .then(date => {
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 
             });
         } else {
-            newsList.innerHTML = "no news to show"
+            newsList.innerHTML = "no news to show ";
         }
         
     
